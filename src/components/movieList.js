f1dde49511030a4aa38867160555929e222cf164
Rahul Hirani <arhirani56@gmail.com>
2019-12-29 23:33:00 +0530

    Update movieList.js

--- src/components/movieList.js
+++ src/components/movieList.js
@@ -1,57 +1,70 @@
-import React from "react";
-import {useState} from "react";
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import React, {Component, useEffect, useState} from 'react';
+import { Button, Modal, Form, FormGroup, Label, Input, ModalBody, ModalHeader, ModalFooter } from 'reactstrap';
 
+const MovieList = ({modal, setmodal}) => {
 
-const movieList = ({modal, showmodal}) => {
-    //const [movie, addMovie] = useState("");
+    const [movie,setmovie] = useState([]);
+
+    useEffect(() => {
+        if(!movie.length)
+        listActors();
+    })
+
+    let listActors = async () => {
+        const list = await fetch("https://jsonplaceholder.typicode.com/users").then(result => result.json());
+        setmovie(list);
+    }
 
     let handleSubmit = (event) => {
-        event.preventDefault();
-        const movieList = JSON.parse(localStorage.getItem("movies")) || [];
-        const movieName = {};
-        movieName.name = event.target.mname.value;
-        movieName.yoy = event.target.yoy.value;
-        movieName.plot = event.target.plot.value;
-        movieName.cast = event.target.cast.value;
-        movieList.push(movieName);
-        localStorage.setItem("movies", JSON.stringify(movieList));
-        showmodal();
+        console.log("inside");
+        let movielist = JSON.parse(localStorage.getItem("movies")) || [];
+        let movie = {};
+        movie.name = event.target.moviename.value;
+        movie.year = event.target.year.value;
+        movie.plot = event.target.plot.value;
+        movie.poster = event.target.poster.value;
+        movie.actor = event.target.cast.value;
+        movielist.push(movie);
+        localStorage.setItem("movies", JSON.stringify(movielist));
     }
 
     return(
-        <Modal isOpen={modal} toggle={showmodal}>
+        <>
+        <Modal isOpen={modal} toggle={setmodal}>
+            <ModalHeader>Add a New Movie</ModalHeader>
             <ModalBody>
-        <form onSubmit={handleSubmit}>
-            <div> Add new Movie</div>
-            <div>Movie Name: 
-                <input name="mname" type="text" />
-            </div>
-            <div className="yor">
-                Year of Release
-                <input name="yoy" type="number"/> 
-            </div>
-            <div className="plot">
-                Plot
-                <input name="plot" type="textarea"/> 
-            </div>
-            <div className="poster">
-                Poster
-                <input name="poster" type="file"/> 
-            </div>
-            <div className="cast">
-                <div>Cast</div>
-                <select name="cast">
-                    <option selected>Tom Cruise</option>
-                    <option>Henry Covill</option>
-                </select>
-            </div>
-            <button type="submit" className="save">Save</button>
-            <button onClick={showmodal} className="cancel">Cancel</button>
-        </form>
-        </ModalBody>
+                <Form id="msubmit" method="GET" onSubmit={handleSubmit}>
+                    <FormGroup>
+                        <Label>MovieName: </Label>
+                        <Input type="text" name="moviename" required/>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label>Year: </Label>
+                        <Input type="number" name="year" required/>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label>Plot: </Label>
+                        <Input type="textarea" name="plot" required/>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label>Poster: </Label>
+                        <Input type="file" name="poster" />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label>Cast: </Label>
+                        <Input type="select" name="cast">
+                            {movie.map((l) => <option name={l.name}>{l.name}</option>)}
+                        </Input>
+                    </FormGroup>
+                </Form>
+            </ModalBody>
+            <ModalFooter>
+                <Button form="msubmit" color="primary" type="submit">Save</Button>{' '}
+                <Button color="secondary" onClick={setmodal}>Cancel</Button>
+            </ModalFooter>
         </Modal>
+        </>
     )
 }
 
-export default movieList;
+export default MovieList;
